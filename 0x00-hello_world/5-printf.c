68d80d4ca60738d5b33220e2cf6ef14d3947589c
marymutheuthomas <marymutheu879@gmail.com>
2023-07-13 06:28:22 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ *
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function.
+ *
+ * Return: Always 0 (Success) main: entry point
+ *
+ * 
+ */
+int main(void)
+{ 
+	printf("with proper grammar, but the outcome is a piece of art,\n ");
+	return (0);
+}
