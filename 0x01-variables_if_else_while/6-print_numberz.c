22dcc9b13df1f5c5e0313f44e7c5ff7c66883f48
marymutheuthomas <marymutheu879@gmail.com>
2023-07-14 06:56:36 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print deciaml 0-10
+ * Return: retunr (0)
+ */
+int main(void)
+{
+	int number = 0;
+	while (number < 10)
+	{
+		print("%d", number);
+		number++;
+	}
+	p
