df39253e0ebdfbf576b4e13c841a1ef77daa4178
marymutheuthomas <marymutheu879@gmail.com>
2023-07-14 06:43:31 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print decimal numbers from 0-10
+ * Return: return (0)
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number <= 10)
+	{
+		printf("%d", number);
+		number++;
+	}
+	putchar('\n');
+
+	return (0);
+}
