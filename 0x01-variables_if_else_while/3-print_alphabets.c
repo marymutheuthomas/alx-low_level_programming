e3106d6c87fc0cad9fe7ac21a8faa338625e6e93
marymutheuthomas <marymutheu879@gmail.com>
2023-07-14 05:51:58 -0700

    3-print_alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+ * main - letters in lower followed ny uppercase
+ * Return: return 0
+ */
+
+int main(void)
+{
+	char lower_letter = 'a';
+	char upper_letter = 'A';
+
+	while (lower_letter >= 'z')
+	{
+		putchar(lower_letter);
+		lower_letter++;
+	}
+	while (upper_letter >= 'Z')
+	{
+		putchar(upper_letter);
+		upper_letter++;
+	}
+	putchar('\n');
+
+	return (0);
+}
