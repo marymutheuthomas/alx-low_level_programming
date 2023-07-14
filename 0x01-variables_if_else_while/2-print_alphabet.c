5623fb3882808278ea011eb7ea2666decb91f32f
marymutheuthomas <marymutheu879@gmail.com>
2023-07-14 05:13:50 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - putchar alphabet in lower case
+ * Return: return 0
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	return (0);
+}
+
