2bd5361da1ea700372e9480a18f17d0b9fedfe70
marymutheuthomas <marymutheu879@gmail.com>
2023-07-16 05:19:52 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<unistd.h>
+/**
+ * main - print a sentece using putchar
+ * Return: return (1)
+ */
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
+}
