623922ec70021292fd7c1273145e4796e727b1e8
marymutheuthomas <marymutheu879@gmail.com>
2023-07-18 11:52:00 -0700

    6-absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * int _abs - calculate the abs of a number
+ * Return: zero
+ * @n: take a rand number
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * (-1);
+	}
+	return (n);
+}
+
