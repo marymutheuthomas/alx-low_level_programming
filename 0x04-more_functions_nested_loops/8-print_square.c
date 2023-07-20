d676989cdda258b04de33655b99f4a130e9aebab
marymutheuthomas <marymutheu879@gmail.com>
2023-07-20 15:18:56 -0700

    8-print_squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include"main.h"
+
+/**
+ * print_square - func to print a square
+ * @size: size of square
+ * Return: no return type
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (i = 0; i <= size; i++)
+	{
+		for (j = 0; j <= size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
+
