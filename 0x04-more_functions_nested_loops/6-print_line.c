9ec545ac660db0c71699a80d43f05fc905e296ae
marymutheuthomas <marymutheu879@gmail.com>
2023-07-20 07:31:03 -0700

    updates

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,23 +3,25 @@
 /**
  * print_line -  function to print a straight line
  * Return: no return type
- * @n - number of times to draw the line
+ * @n: number of times to draw the line
  */
 
 void print_line(int n)
 {
 	int i = 0;
-	 if ( n < 0)
-	 {
-		 _putchar('\n');
-	 }
-	 else{
+
+	if (n < 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
 		while (i <= n)
 		{
 			_putchar('_');
 			i++;
 		}
 		_putchar('\n');
-	 }
+	}
 }
 
