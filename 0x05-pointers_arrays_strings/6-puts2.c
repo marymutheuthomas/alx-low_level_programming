f2d265edb76285724232bbde6c963deb5db5780a
marymutheuthomas <marymutheu879@gmail.com>
2023-07-25 04:48:16 -0700

    6-puts.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,7 +11,12 @@ void puts2(char *str)
 {
 	int i;
 
-	for(i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-
-		
+		if (str[i] % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
