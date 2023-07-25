e91325178a430692bfd4b1ff6fef8bc9477476f0
marymutheuthomas <marymutheu879@gmail.com>
2023-07-24 22:44:11 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include<string.h>
+
+/**
+ * rev_string - to reverse a string
+ * @s: string to reverse
+ */
+
+void rev_string(char *s)
+{
+	int length = strlen(s);
+	int i, j;
+	char temp;
+
+	for (i = 0, j = length - 1; i < j; i++, j--)
+	{
+	temp = s[i];
+	s[i] = s[j];
+	s[j] = temp;
+	}
+}
