8e04e3f2173a79a55ef1a264eaf8449910d2a995
marymutheuthomas <marymutheu879@gmail.com>
2023-07-27 00:54:15 -0700

    2-strcpy.c updated

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,12 +14,10 @@ char *_strncpy(char *dest, char *src, int n)
 	int i, length;
 
 	length = strlen(dest);
-	i = 0;
 
-	while (i < n && src[i] != '\0')
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
 
 	while (i < n)
