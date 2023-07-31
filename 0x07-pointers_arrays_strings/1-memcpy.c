d87999a1dccc8f26c6a97ebc9d0dd27948a37673
marymutheuthomas <marymutheu879@gmail.com>
2023-07-31 07:36:13 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include"main.h"
+#include<string.h>
+
+/**
+ * _memcpy - function to cpy n bytes from src to dest
+ * @dest: dest to cpy memory to
+ * @src: source dest to copy memory from
+ * @n: bytes from memory
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	int length;
+
+	length = strlen(dest);
+
+	for (i = 0; i < n; i++)
+	{
+		dest[length + i] = src[i];
+	}
+	return (dest);
+}
+
