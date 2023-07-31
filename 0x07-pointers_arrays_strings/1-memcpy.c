aa4eb404d771603e62d8acb897f5a7ad2901115e
marymutheuthomas <marymutheu879@gmail.com>
2023-07-31 07:39:22 -0700

    1-memcpy.c updated

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,13 +12,10 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	int length;
-
-	length = strlen(dest);
 
 	for (i = 0; i < n; i++)
 	{
-		dest[length + i] = src[i];
+		dest[i] = src[i];
 	}
 	return (dest);
 }
