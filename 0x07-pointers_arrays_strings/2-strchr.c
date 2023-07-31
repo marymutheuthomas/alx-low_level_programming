a1be1bf279e1189afd0bd99abf1a09e4fc4bb60a
marymutheuthomas <marymutheu879@gmail.com>
2023-07-31 07:54:48 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * _strchr - function to find char in a string
+ * @s: pointer to string s
+ * @c: charater to locate
+ * Return: return pointer to first occurence of c
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + 1);
+		}
+	}
+	return (0);
+}
