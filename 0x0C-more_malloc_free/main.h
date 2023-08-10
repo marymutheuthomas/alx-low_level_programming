7a0dac085cd5895f5d50d96fb9cf87c7f8b888c6
marymutheuthomas <marymutheu879@gmail.com>
2023-08-10 08:48:54 -0700

    0-malloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,32 +1,8 @@
 #ifndef MAIN_H
-#define MAIN_H
+#define _MAIN_H
 
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 
-#endif /* MAIN_H */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#endif 
