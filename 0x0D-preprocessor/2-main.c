9886493bd23286146b0a2fc64411fd8183b0d6dc
marymutheuthomas <marymutheu879@gmail.com>
2023-08-14 06:51:42 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 
 /**
- * void - function to print program name
+ * main - function to print program name
  * @argc: argument count
  * @argv: argument vector
  * Return: return (0)
  */
 
-int main(int argc __attribute__((unused)), char* argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
 
