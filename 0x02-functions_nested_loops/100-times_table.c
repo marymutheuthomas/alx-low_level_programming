efb9089d41f6c076dbe32206b40a7c49fa8c3df1
marymutheuthomas <marymutheu879@gmail.com>
2023-08-15 01:35:43 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,68 +1,41 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * print_number - print a number with proper formatting
- * @num: the number to print
- * Return: void
- */
-void print_number(int num)
-{
-	if (num < 10)
-	{
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(' ');
-	}
-	else if (num < 100)
-	{
-		_putchar(' ');
-		_putchar(' ');
-	}
-	if (num >= 100)
-	{
-		_putchar(num / 100 + '0');
-		_putchar((num / 10) % 10 + '0');
-		_putchar(num % 10 + '0');
-	}
-	else if (num >= 10)
-	{
-		_putchar(' ');
-		_putchar(num / 10 + '0');
-		_putchar(num % 10 + '0');
-	}
-	else
-	{
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(num + '0');
-	}
-}
+ * print_times_table - prints time table of n
+ *
+ * @n: takes number input
+*/
 
-/**
- * print_times_table - print n times table
- * @n: max n times table
- * Return: void
- */
 void print_times_table(int n)
 {
-	int i, j, product;
+	int prod, mult, num;
 
 	if (n <= 15 && n >= 0)
 	{
-		for (i = 0; i <= n; i++)
+		for (num = 0; num <= n; ++num)
 		{
-			for (j = 0; j <= n; j++)
+			_putchar(48);
+			for (mult = 1; mult <= n; ++mult)
 			{
-				product = j * i;
-				if (j > 0)
-				{
-					_putchar(',');
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 9)
 					_putchar(' ');
-				}
-				print_number(product);
+				if (prod <= 99)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + 48);
+					_putchar((prod / 10) % 10 + 48);
+				} else if (prod <= 99 && prod >= 10)
+					_putchar((prod / 10) + 48);
+				_putchar((prod % 10) + 48);
 			}
 			_putchar('\n');
 		}
 	}
 }
-
