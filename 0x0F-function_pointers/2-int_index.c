4c81a698b2b05700b0706f015fb3b1bec2f597df
marymutheuthomas <marymutheu879@gmail.com>
2023-08-16 07:09:07 -0700

    betty coding style issue updated

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,26 +6,24 @@
  * @array: pointer to the array
  * @size: size of the array
  * @cmp: pointer to function compare
- * Return: Index of the first element for which cmp function does not return 0, or -1
+ * Return: Index of the first element
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-        int i;
+	int i;
 
 	if (size <= 0)
 	{
 		return (-1);
 	}
-
-        if (array != NULL && cmp != NULL)
+	if (array != NULL && cmp != NULL)
 	{
-		for  (i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 		{
-			y = cmp(array[i]) != 0;
-			if ( y == TRUE)
+			if (cmp(array[i]) != 0)
 			{
-				return i;
+				return (i);
 			}
 		}
 	}
