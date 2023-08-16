012a36b8d5676d51c0ce53bc63631b2fbf270a90
marymutheuthomas <marymutheu879@gmail.com>
2023-08-16 05:30:16 -0700

    array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include"function_pointers.h"
+#include <stddef.h>
+
+/**
+ * array_iterator - prints array in int & hexdecimal
+ * @action: pointer to a function to iterate
+ * @array: pointer to the array
+ * @size: size of the array
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array != NULL && action != NULL && size > 0)
+		for (i = 0; i <= size; i++)
+			action(array[i]);
+}
+
