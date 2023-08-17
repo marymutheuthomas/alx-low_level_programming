fa56fecb581386e72d4d97b64e5d6ffbcf5972a4
marymutheuthomas <marymutheu879@gmail.com>
2023-08-17 06:14:07 -0700

    updated output expectation

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			printf("%d", num);
 			if (i < n - 1)
 			{
-				printf("%s", separator);
+				printf("%s ", separator);
 			}
 		}
 	}
