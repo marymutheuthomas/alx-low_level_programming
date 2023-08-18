4b42cef4826e41803eafd953312a5f9d420b97cb
marymutheuthomas <marymutheu879@gmail.com>
2023-08-18 05:53:12 -0700

    2-print_strings.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,18 +12,17 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list ap;
-	unsigned int index;
+	va_list mylist;
+	unsigned int i;
 
-	/* initialize the argument list from the start */
-	va_start(ap, n);
+	va_start(mylist, n);
 
-	for (index = 0; index < n; index++)
+	for (i= 0; i < n; i++)
 	{
-		printf("%d", va_arg(ap, int));
+		printf("%d", va_arg(mylist, int));
 		if (separator && index != n - 1)
 			printf("%s", separator);
 	}
-	va_end(ap);
+	va_end(mylist);
 	printf("\n");
 }
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include<stdarg.h>
+#include<stdio.h>
+#include<stddef.h>
+
+/**
+ * print_strings - function to print strings 
+ * @separator: to separate strings
+ * @n:number of strings passed to the function
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list mylist;
+	char *item;
+
+	va_start(mylist, n);
+	for (i = 0; i < n; i++)
+	{
+		item = va_arg(mylist, char *);
+		if (item == NULL)
+		{
+			printf("nil");
+		}
+		else
+		{
+			printf("%s", (const char *)item);
+		}
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(mylist);
+	printf("\n");
+}
+
