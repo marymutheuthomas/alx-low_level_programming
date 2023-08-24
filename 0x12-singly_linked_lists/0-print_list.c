21fc493ea987d4ebe2e030d814449b98fa29babd
marymutheuthomas <marymutheu879@gmail.com>
2023-08-24 05:58:53 -0700

    traversing and printing

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include"lists.h"
+#include<stdlib.h>
+
+/**
+ * print_list - futcion to print the elements of a linked list
+ * @h: pointer to the head node
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+	const list_t *current = h;
+
+	while (current != NULL)
+	{
+		if (current->len == 0)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n",current->len, current->str);
+			count++;
+			current = current->next;
+		}
+	}
+	return (count);
+}
+
