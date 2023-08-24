5a48ffda849e11bbe3442065f0f984848f3da942
marymutheuthomas <marymutheu879@gmail.com>
2023-08-24 07:08:50 -0700

    2-linkedlist len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include"lists.h"
+
+/**
+ * list_len - function to print the length of a linked list
+ * @h: pointer to linked list head
+ * Return: size of linked list
+ */
+
+size_t list_len(const list_t *h)
+{
+	int nodeCount = 1;
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		h = h->next;
+	}
+	nodeCount++;
+	return (nodeCount);
+}
