8a1f5fa80eb03c1c6058187eff1296fcaa600d2e
marymutheuthomas <marymutheu879@gmail.com>
2023-08-24 07:22:42 -0700

    updates

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,14 +8,14 @@
 
 size_t list_len(const list_t *h)
 {
-	int nodeCount = 1;
+	int nodeCount = 0;
 
 	if (h == NULL)
 		return (0);
 	while (h != NULL)
 	{
 		h = h->next;
+		nodeCount++;
 	}
-	nodeCount++;
 	return (nodeCount);
 }
