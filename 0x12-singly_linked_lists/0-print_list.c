5cf591193dd81b015e0ca3e582d4f601172928ce
marymutheuthomas <marymutheu879@gmail.com>
2023-08-24 06:07:26 -0700

    0-printlist.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -21,7 +21,7 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("[%d] %s\n",current->len, current->str);
+			printf("[%d] %s\n", current->len, current->str);
 			count++;
 		}
 		current = current->next;
