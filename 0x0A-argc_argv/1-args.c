391a48c5440e9264a6e77a432532652cf4765c89
marymutheuthomas <marymutheu879@gmail.com>
2023-09-03 06:11:53 -0700

    source code description

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,15 +2,14 @@
 
 /**
  * main - main fucntion
- * argc: argument count
- * argv: argument vector
+ * @argc: argument count
+ * @argv: argument vector
  * Return: return (0)
  */
 
 int main(int argc, char __attribute__((unused)) *argv[])
 {
 	printf("%d\n", argc);
-	
 	return (0);
 }
 
