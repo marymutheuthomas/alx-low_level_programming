18f7d867834cce51df538f7a9684989d3f2364aa
marymutheuthomas <marymutheu879@gmail.com>
2023-09-03 05:48:34 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - main fucntion
+ * argc: argument count
+ * argv: argument vector
+ * Return: return (0)
+ */
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	
+	return (0);
+}
+
