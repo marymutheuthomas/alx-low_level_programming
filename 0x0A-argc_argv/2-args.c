5dd148771c178f206aef3d725e53ed8056898f15
marymutheuthomas <marymutheu879@gmail.com>
2023-09-03 06:31:12 -0700

    updated source code

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
+
