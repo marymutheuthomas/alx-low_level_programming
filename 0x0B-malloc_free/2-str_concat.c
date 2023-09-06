6786d2ce129e8b8195c338b3e83f769de7c86c56
marymutheuthomas <marymutheu879@gmail.com>
2023-09-05 22:18:19 -0700

    string null handling updated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,9 +15,12 @@ char *str_concat(char *s1, char *s2)
 	unsigned int total_len;
 	char *new_memory;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
 		s1 = "";
+	}
+	if (s2 == NULL)
+	{
 		s2 = "";
 	}
 	length_s1 = strlen(s1);
