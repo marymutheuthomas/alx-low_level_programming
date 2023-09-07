2d01efbafaed2af0a7d4b247de0d3e2cb61d5659
marymutheuthomas <marymutheu879@gmail.com>
2023-09-07 06:56:40 -0700

    custom calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ * _calloc - function that allocates memory for an array, using malloc
+ * @nmemb: embers of the array
+ * @size: size of the array
+ * Return: pointer to the new allocvated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *my_array;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	my_array = malloc(nmemb * size);
+	if (my_array == NULL)
+	{
+		return (NULL);
+	}
+	return (my_array);
+}
+
