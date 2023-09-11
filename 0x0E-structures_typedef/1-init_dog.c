5dac8bef84d736b397930ae875ae08fe90226fad
marymutheuthomas <marymutheu879@gmail.com>
2023-09-11 14:11:23 -0700

    fixed code bug issue

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,9 +10,12 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog mydog = d;
-	mydog->name = name;
-	mydog->age = age;
-	mydog->owner = owner;
+	if (d == NULL)
+	{
+		return;
+	}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
 
