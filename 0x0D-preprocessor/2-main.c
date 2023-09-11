2ab562bee0d3dad92775a90e6e2bf9f015ae2346
marymutheuthomas <marymutheu879@gmail.com>
2023-09-10 22:53:59 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#define _unused_ __attribute__((unused))
+
+/**
+ * main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: (0)
+ */
+
+int main(int argc _unused_, char *argv[])
+{
+	char *prog_name;
+	
+	prog_name = argv[0];
+	return (0);
+}
+
