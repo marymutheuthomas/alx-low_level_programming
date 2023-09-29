23035b37668150221a53c89cb6131e63ff988c93
marymutheuthomas <marymutheu879@gmail.com>
2023-09-29 03:11:49 -0700

    fixed betty

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -4,7 +4,6 @@
  * clear_bit - Sets the value of a bit to 0 at a given index
  * @n: A pointer to the unsigned long int to modify
  * @index: The index of the bit to clear, starting from 0
- * 
  * Return: 1 if it worked, or -1 if an error occurs
  */
 
