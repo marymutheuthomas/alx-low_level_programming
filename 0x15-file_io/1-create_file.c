a141cd82fbd0b4210d5975c898d5fafea2d6d258
marymutheuthomas <marymutheu879@gmail.com>
2023-10-02 14:33:10 -0700

    create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file - function to create file
  * @filename: pointer to file
- * @text_content - text data
+ * @text_content: text data
  * Return: -1 or 0
  */
 
@@ -14,23 +14,23 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 	{
-		return -1;
+		return (-1);
 	}
-	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (file_descriptor == -1)
+	file_des = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file_des == -1)
 	{
-		return -1;
+		return (-1);
 	}
 	if (text_content != NULL)
 	{
-		bytes_written = write(file_descriptor, text_content, strlen(text_content));
+		bytes_written = write(file_des, text_content, strlen(text_content));
 		if (bytes_written == -1)
 		{
-			close(file_descriptor);
-			return -1;
+			close(file_des);
+			return (-1);
 		}
 	}
-	close(file_descriptor);
-	return 1;
+	close(file_des);
+	return (1);
 }
 
